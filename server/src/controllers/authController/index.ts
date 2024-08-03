f80f889571a80276afeba40d01334429fcc3ec12
montherismail <montherismail90@gmail.com>
2024-08-03 15:57:34 +0300

    create logout functionality Relates #9

--- server/src/controllers/authController/index.ts
+++ server/src/controllers/authController/index.ts
@@ -1,5 +1,6 @@
 import signUp from "./signUp";
 import getUsers from "./getUsers";
 import login from "./login";
+import logout from "./logout";
 
-export { signUp, getUsers, login};
+export { signUp, getUsers, login, logout };
