5796abeeacffab875b51b8c9e5ee6a3d0ed90ac5
montherismail <montherismail90@gmail.com>
2024-08-07 13:26:39 +0300

    create edit profile functionality Relates #26

---
+++ server/src/controllers/profileControllers/editProfile.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from "express";
+import { editProfileSchema } from "../../utils/validation";
+import prisma from "../../middleware/prisma";
+import { createToken } from "../../utils/jwt";
+
+const editProfile = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const { userEmail, userName } = req.body;
+  await editProfileSchema.validateAsync(req.body);
+  try {
+    const data = await prisma.users.findUnique({
+      where: { id: parseInt(id) },
+    });
+    if (!data) {
+      return res.status(404).json({ message: "No User Found" });
+    }
+    const { role } = data;
+    const emailData = await prisma.users.findUnique({
+      where: { email: userEmail },
+    });
+    if (!emailData) {
+      return res
+        .status(422)
+        .json({ message: "email already exist, try another one" });
+    }
+    const updatedProfile = await prisma.users.update({
+      where: { id: parseInt(id) },
+      data: {
+        email: userEmail,
+        username: userName,
+      },
+    });
+
+    const token = await createToken({
+      id,
+      role,
+      userName,
+    });
+    return res
+      .status(201)
+      .cookie("token", token, {
+        httpOnly: false,
+      })
+      .json({ message: "Profile edited Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+};
+
+export default editProfile;
--- server/src/routes/profileRoutes.ts
+++ server/src/routes/profileRoutes.ts
@@ -1,9 +1,10 @@
 import Router from "express";
-import { getProfile } from "../controllers/profileControllers";
+import { getProfile, editProfile } from "../controllers/profileControllers";
 import asyncMiddleware from "../middleware/asyncMiddleware";
 
 const router = Router();
 
 router.get("/:id", asyncMiddleware(getProfile));
+router.put("/:id", asyncMiddleware(editProfile));
 
 export default router;
