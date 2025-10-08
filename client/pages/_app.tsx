6bca9b4eaf50501c71d3bc5ab65d9d384cd6c428
montherismail <montherismail90@gmail.com>
2025-10-08 11:41:07 +0300

    fix: Force light mode - prevent browser dark mode color changes

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -16,13 +16,14 @@ export default function App({ Component, pageProps }: AppProps) {
         <title>E-Commerce Store</title>
         <meta name="description" content="Your premier online shopping destination" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="color-scheme" content="light only" />
         <link rel="icon" href={`${basePath}/favicon.svg?v=9`} type="image/svg+xml" />
         <link rel="icon" href={`${basePath}/favicon.ico?v=9`} />
         <link rel="apple-touch-icon" href={`${basePath}/apple-touch-icon.png?v=9`} />
         <link rel="icon" type="image/png" sizes="192x192" href={`${basePath}/icon-192.png?v=9`} />
         <link rel="icon" type="image/png" sizes="512x512" href={`${basePath}/icon-512.png?v=9`} />
         <link rel="manifest" href={`${basePath}/manifest.json`} />
-        <meta name="theme-color" content="#000000" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <AuthProvider>
         <CartProvider>
